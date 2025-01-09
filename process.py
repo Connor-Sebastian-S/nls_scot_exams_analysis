8c212e4a871a4677a7b054ccbcb8a8239743315a
ConnorS <35559977+Connor-Sebastian-S@users.noreply.github.com>
2025-01-09 21:37:35 +0000

    Sentiment analysis

--- process.py
+++ process.py
@@ -6,6 +6,136 @@ import matplotlib.pyplot as plt
 import numpy as np
 from genbit.genbit_metrics import GenBitMetrics
 import pprint
+import requests
+import nltk
+import regex as re
+import pandas as pd
+from wordcloud import WordCloud
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+
+from nltk.sentiment import SentimentIntensityAnalyzer
+from nltk.stem import WordNetLemmatizer
+
+
+def tokenize_text(text: str):
+    
+    # lowercase the text
+    text = text.lower()
+    
+    # remove punctuation from text
+    text = re.sub(r"[^\w\s]", "", text)
+    
+    # tokenize the text
+    tokens = nltk.word_tokenize(text)
+    
+    # remove stopwords from txt_tokens and word_tokens
+    from nltk.corpus import stopwords
+    english_stop_words = set(stopwords.words('english'))
+    tokens = [word for word in tokens if word not in english_stop_words]
+    
+    # return your tokens
+    return tokens
+
+def lemmatize_tokens(tokens):
+    
+    # initiate lemmatizer
+    lemmatizer = WordNetLemmatizer()
+    
+    # lemmatize tokens
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_tokens = [lemmatizer.lemmatize(word) for word in tokens]
+    
+    # return your lemmatized tokens
+    return lemmatized_tokens
+
+# return the most common tokens
+def return_top_tokens(tokens,
+                      top_N = 10):
+
+    # first, count the frequency of every unique token
+    word_token_distribution = nltk.FreqDist(tokens)
+    
+    # next, filter for only the most common top_N tokens
+    # also, put this in a dataframe
+    top_tokens = pd.DataFrame(word_token_distribution.most_common(top_N),
+                              columns=['Word', 'Frequency'])
+    
+    # return the top_tokens dataframe
+    return top_tokens
+
+
+# return the most common bi-grams
+from nltk.collocations import BigramCollocationFinder
+
+def return_top_bigrams(tokens,
+                       top_N = 10):
+    
+    # collect bigrams
+    bcf = BigramCollocationFinder.from_words(tokens)
+    
+    # put bigrams into a dataframe
+    bigram_df = pd.DataFrame(data = bcf.ngram_fd.items(),
+                             columns = ['Bigram', 'Frequency'])
+    
+    # sort the dataframe by frequency
+    bigram_df = bigram_df.sort_values(by=['Frequency'],ascending = False).reset_index(drop=True)
+    
+    # filter for only top bigrams
+    bigram_df = bigram_df[0:top_N]
+    
+    # return the bigram dataframe
+    return bigram_df
+
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+def return_sentiment_df(tokens):
+
+	# initialize sentiment analyzer
+	sia = SentimentIntensityAnalyzer()
+	
+	# create some counters for sentiment of each token
+	positive_tokens = 0
+	negative_tokens = 0
+	neutral_tokens = 0
+	compound_scores = []
+		
+	# loop through each token
+	for token in tokens:
+		
+		if sia.polarity_scores(token)["compound"] > 0:
+			
+			positive_tokens += 1
+			compound_scores.append(sia.polarity_scores(token)["compound"])
+			
+		elif sia.polarity_scores(token)["compound"] < 0:
+			
+			negative_tokens += 1
+			compound_scores.append(sia.polarity_scores(token)["compound"])
+			  
+		elif sia.polarity_scores(token)["compound"] == 0:
+			
+			neutral_tokens += 1
+			compound_scores.append(sia.polarity_scores(token)["compound"])
+      
+	
+			
+	# put sentiment results into a dataframe
+	compound_score_numbers = [num for num in compound_scores if num != 0]
+	csc = 0
+	if len(compound_score_numbers) > 0:
+		csc = sum(compound_score_numbers) / len(compound_score_numbers)
+	sentiment_df = pd.DataFrame(data = {"total_tokens" : len(tokens),
+										"positive_tokens" : positive_tokens,
+										"negative_tokens" : negative_tokens,
+										"neutral_tokens" : neutral_tokens,
+										"compound_sentiment_score" : csc},
+								index = [0])
+
+	# return sentiment_df
+	return sentiment_df
+
 
 def process_exam_text(file_path):
     """
@@ -43,6 +173,8 @@ def process_exam_text(file_path):
         print(f"Error processing file {file_path}: {e}")
         return None
 
+
+    
 def Analyse_and_save_questions(metadata, output_file):
     """
     Analyse questions and save results to a CSV file.
@@ -52,7 +184,11 @@ def Analyse_and_save_questions(metadata, output_file):
     textstat.set_lang("en_GB")
     rows = []
     
-    #genbit_metrics_object = GenBitMetrics("EN", context_window=5, distance_weight=0.95, percentile_cutoff=80)
+    def print_pred(texts, pred):
+        for txt, p in zip(texts, pred):
+            print("formal: ", txt) if p == 0 else print("informal: ", txt)
+            
+
 
     for q in metadata["questions"]:
         main_question_number = q[0]  # e.g., "1.", "2."
@@ -71,6 +207,10 @@ def Analyse_and_save_questions(metadata, output_file):
             
         main_question_number = remove_format(main_question_number)
         
+        tokens = tokenize_text(text = main_question_text)
+        from nltk.stem import WordNetLemmatizer
+        lemmatized_tokens = lemmatize_tokens(tokens = tokens)
+        sentiment_df = return_sentiment_df(tokens = lemmatized_tokens)
         
         rows.append({
             "year": metadata["year"],
@@ -80,7 +220,12 @@ def Analyse_and_save_questions(metadata, output_file):
             "text": main_question_text,
             "coleman_liau": main_score,
             "flesch_kincaid": flesch_kincaid_grade,
-            "gunning_fog": gunning_fog
+            "gunning_fog": gunning_fog,
+            "total_tokens" : sentiment_df["total_tokens"],
+            "positive_tokens" : sentiment_df["positive_tokens"],
+            "negative_tokens" : sentiment_df["negative_tokens"],
+            "neutral_tokens" : sentiment_df["neutral_tokens"],
+            "compound_sentiment_score" : sentiment_df["compound_sentiment_score"]
         })
 
         # Analyse subquestions
@@ -92,6 +237,12 @@ def Analyse_and_save_questions(metadata, output_file):
             sub_score = textstat.coleman_liau_index(subtext.strip()) 
             sub_flesch_kincaid_grade = textstat.flesch_kincaid_grade(subtext.strip())
             sub_gunning_fog = textstat.gunning_fog(subtext.strip())
+            
+            tokens = tokenize_text(text = subtext)
+            from nltk.stem import WordNetLemmatizer
+            lemmatized_tokens = lemmatize_tokens(tokens = tokens)
+            sentiment_df = return_sentiment_df(tokens = lemmatized_tokens)     
+
             rows.append({
                 "year": metadata["year"],
                 "level": metadata["level"],
@@ -100,16 +251,38 @@ def Analyse_and_save_questions(metadata, output_file):
                 "text": subtext.strip(),
                 "coleman_liau": sub_score,
                 "flesch_kincaid": sub_flesch_kincaid_grade,
-                "gunning_fog": sub_gunning_fog
+                "gunning_fog": sub_gunning_fog,
+                "total_tokens" : sentiment_df["total_tokens"],
+                "positive_tokens" : sentiment_df["positive_tokens"],
+                "negative_tokens" : sentiment_df["negative_tokens"],
+                "neutral_tokens" : sentiment_df["neutral_tokens"],
+                "compound_sentiment_score" : sentiment_df["compound_sentiment_score"]
             })
             
     #for idx, q in enumerate([row['text'] for row in rows]): 
+        #print(q)
+        #tokens = tokenize_text(text = q)
+
+        #from nltk.stem import WordNetLemmatizer
+       # lemmatized_tokens = lemmatize_tokens(tokens = tokens)
+
+        #top_tokens = return_top_tokens(tokens = lemmatized_tokens,
+                               #top_N = 10)
+        #print(top_tokens)
+
+        # run the return_top_bigrams function and print the results
+        #bigram_df = return_top_bigrams(tokens = lemmatized_tokens,
+                               #top_N = 10)
+        #print(bigram_df)
+
+        #sentiment_df = return_sentiment_df(tokens = lemmatized_tokens)
+        #print(sentiment_df)
 
 
         
     # Write to CSV
     with open(f"{output_file}_results.csv", 'w', newline='', encoding='utf-8') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=["year", "level", "subject", "question", "text", "coleman_liau", "flesch_kincaid", "gunning_fog"])
+        writer = csv.DictWriter(csvfile, fieldnames=["year", "level", "subject", "question", "text", "coleman_liau", "flesch_kincaid", "gunning_fog", "total_tokens", "positive_tokens", "negative_tokens", "neutral_tokens", "compound_sentiment_score"])
         writer.writeheader()
         writer.writerows(rows)
         
@@ -123,6 +296,17 @@ def Analyse_and_save_questions(metadata, output_file):
     plt.ylabel('Gunning Fog score')
     plt.title(f"{metadata["year"]} - {metadata["level"]} - {metadata["subject"]} - Gunning Fog score")
     plt.savefig(f"{output_file}_fog.png")
+    plt.clf()
+
+    print(f"Saved results to {output_file}")
+    
+    compound_sentiment_value = np.array([row['compound_sentiment_score'] for row in rows])
+    plt.plot(question_id, compound_sentiment_value, 'o-')
+    plt.xticks(question_id, question_labels)
+    plt.xlabel('Question')
+    plt.ylabel('Compound Sentiment score')
+    plt.title(f"{metadata["year"]} - {metadata["level"]} - {metadata["subject"]} - Compound Sentiment score")
+    plt.savefig(f"{output_file}_css.png")
     plt.clf()
 
     print(f"Saved results to {output_file}")
@@ -144,7 +328,10 @@ def process_all_files(folder_path, output_dir):
         Analyse_and_save_questions(metadata, output_file)
 
 # Example usage
+
+#nltk.download()
 folder_path = './data/text/test/'  # Directory containing exam text files
 output_dir = './output/'     # Directory to save CSV files
 os.makedirs(output_dir, exist_ok=True)
+        
 process_all_files(folder_path, output_dir)
